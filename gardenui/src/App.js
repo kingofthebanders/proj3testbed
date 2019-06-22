ec892c3830eb8c1d190d859e540dc419263c22b2
Ben Andersen <benjamin.andersen@alumni.nd.edu>
2019-06-22 13:21:35 -0500

    latest

---
+++ gardenui/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ShowcaseLayout from './components/ShowcaseLayout';
+
+function App() {
+  return (
+    <div className="App">
+    <ShowcaseLayout />
+    </div>
+  );
+}
+
+export default App;
